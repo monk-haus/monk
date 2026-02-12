6b6add5fba6100b15cc1429aa643a4bb643faeee
monk-haus <hello@monk.haus>
2026-02-12 22:16:38 +0200

    form fix

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import ContactForm from "@/components/ContactForm";
 
+export const runtime = 'edge';
+
 export const metadata: Metadata = {
     title: "Contact",
     description: "Initiate contact. Project inquiries and collaboration requests.",
