5278f24c101102e1c47e855b47c11cb83b0f35b0
monk-haus <hello@monk.haus>
2026-07-07 20:05:02 +0300

    Update work.ts

--- app/work.ts
+++ app/work.ts
@@ -7,8 +7,9 @@ export type WorkItem = {
 
 export const work: WorkItem[] = [
   { title: "Samuel Bristow", url: "https://samuelbristow.com", services: "Website", year: "2026" },
+  { title: "Anders Sølvsten Thomsen", url: "https://anderssoelvstenthomsen.com", services: "Website", year: "2026" },
   { title: "Pixiesmith", url: "https://pixiesmith.studio", services: "Website", year: "2026" },
-  { title: "Gaplens", url: "https://gaplens.com", services: "Website & Brand Identity", year: "2025" },
+  { title: "Gaplens", url: "https://gaplens.com", services: "Website & Brand Identity", year: "2026" },
   { title: "Naath Models", url: "https://naathmodels.com", services: "Website & Brand Identity", year: "2026" },
   { title: "Vault", url: "https://thevaulthq.io", services: "Website", year: "2026" },
   { title: "sort.lat", url: "https://sort.lat", services: "Website & Brand Identity", year: "2026" },
